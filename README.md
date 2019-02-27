e641be2d9c3f50da09fe2fa7c184141198396570
AlfJohnFred <alfred4993@gmail.com>
2019-02-27 12:48:05 +0000

    -Completed retrieval of album art for Genre : metal -Takes list of artists from text file. -Need text files of artists to complete rest of genres -Retry mechanism to handle HTTP timeouts not working as intended. -Code status: V1 Completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# genreFromAlbumCover
+# Get genre From Album Covers
 Classify albums by genre based on album cover
--- getAlbumArt.py
+++ getAlbumArt.py
@@ -1,8 +1,11 @@
+import errno
 import requests
 import os
 import spotipy
 import spotipy.util as util
 from Config import CLIENT_ID, CLIENT_SECRET
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
@@ -15,10 +18,10 @@ def get_artist(name):
     if len(items) > 0:
         return items[0]
     else:
-        return None
+        return -1
 
 
-def save_album_art(id,folder_name):
+def save_album_art(id, folder_name):
     albums = []
     results = sp.artist_albums(artist['id'], album_type='album')
     albums.extend(results['items'])
@@ -26,34 +29,47 @@ def save_album_art(id,folder_name):
         results = sp.next(results)
         albums.extend(results['items'])
     print('Total albums:', len(albums))
-    unique = set()  # skip duplicate albums
     for album in albums:
-        name = album['name'].lower()
         for images in album['images']:
             if images['height'] == 640:
                 url = (images['url'])
-                filename = "D:\\CodePlayPen\\Python\\genreFromAlbumCover\\" + folder_name+"\\" + url.split('/')[4]+".jpeg"
-                album_art = requests.get(url, allow_redirects=True)
-                print(filename)
+                print(url)
+                filename = "D:\\CodePlayPen\\Python\\genreFromAlbumCover\\" + folder_name + "\\" + url.split('/')[
+                    4] + ".jpeg"
+
+                s = requests.Session()
+                retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
+                s.mount('http://', HTTPAdapter(max_retries=retries))
+                album_art = s.get(url, allow_redirects=True)
+
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
                 open(filename, 'wb').write(album_art.content)
 
 
-def show_artist(artist):
+def create_destination_folder(artist, genre):
     print('====', artist['name'], '====')
     print('Popularity: ', artist['popularity'])
     if len(artist['genres']) > 0:
         print('Genres: ', ','.join(artist['genres']))
-    filename = "/"+artist['name']
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    return artist['name']
+    folder = "D:\\CodePlayPen\\Python\\genreFromAlbumCover\\" + genre
+    try:
+        os.makedirs(folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
 if __name__ == '__main__':
     sp = spotipy.Spotify(cache_token)
     sp.trace = False
 
-    name = 'metallica'
-    artist = get_artist(name)
-    folder_name = show_artist(artist)
-    save_album_art(artist,folder_name)
+    with open("metal.txt", 'r') as f:
+        names = [line.strip() for line in f]
+        genre = "Metal"
+    for name in names:
+        artist = get_artist(name)
+        if artist != -1:
+            folder_name = create_destination_folder(artist, genre)
+            save_album_art(artist, genre)
+        else:
+            print("No data for"+name)
